bcaff11da17081eb7fd85fe0455a3936face3573
Sagar Pratapsi <sagar.pratapsi@meaningfuldata.eu>
2020-12-06 18:58:06 +0000

    correction

--- 6.py
+++ 6.py
@@ -5,7 +5,7 @@ def group_lines(file):
     """Clean and group lines (divided by blank lines)."""
     clean_lines = (line.strip() for line in file)
     groups = it.groupby(clean_lines, key=bool)
-    yield from (lines for is_blank, lines in groups if not is_blank)
+    return (lines for is_not_blank, lines in groups if is_not_blank)
 
 def union(sets):
     return ft.reduce(set.union, sets)
@@ -15,7 +15,7 @@ def intersection(sets):
 
 with open('6.in') as file:
     groups = group_lines(file)
-    group_answers = [set(line) for line in groups]
+    group_answers = [[set(line) for line in group] for group in groups]
 
 part1 = sum(len(union(answers)) for answers in group_answers)
 part2 = sum(len(intersection(answers)) for answers in group_answers)
